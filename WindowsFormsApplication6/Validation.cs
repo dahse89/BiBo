ab22ba0780d0c9b0e851f558a0f31854a7a73961
Yevgen Korepin <stabest@hotmail.de>
2013-10-25 13:59:42 +0200

    Added Validation Class

---
+++ WindowsFormsApplication6/Validation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;  
+
+namespace BiBo
+{
+  class Validation
+  {
+    public static bool isEmpty(string s) // Leer oder Null
+    {
+      return String.IsNullOrEmpty(s.Trim());
+    }
+    public static bool isNumeric(string s) // Zahlen
+    {
+      return Regex.IsMatch(s, "^[0-9]+$");  
+    }
+    public static bool isAlphabetic(string s) // Alphabetisch
+    {
+      return Regex.IsMatch(s, "^[A-Za-zäÄöÖüÜß\\s]+$"); 
+    }
+    public static bool Name(string s)
+    {
+      return (!isEmpty(s) && isAlphabetic(s));
+    }
+    public static bool Street(string s)
+    {
+      return (!isEmpty(s) && (isAlphabetic(s) || isNumeric(s)));
+    }
+    public static bool zipCode(string s)
+    {
+      return true; // @ todo
+    }
+
+  }
+}
