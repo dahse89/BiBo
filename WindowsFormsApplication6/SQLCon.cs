a7d3fc04bd840239f5eb581aa907d695b827970b
Vico Dambeck <vico.dambeck@gmail.com>
2013-10-29 12:39:01 +0100

    Added first Tastcase

--- WindowsFormsApplication6/SQLCon.cs
+++ WindowsFormsApplication6/SQLCon.cs
@@ -11,6 +11,7 @@ namespace BiBo
   public class SQLCon
   {
       private SQLiteConnection con;
+
       // Initialisiert die "Datenbank"
       // Sorgt dafür, dass bei jedem Aufruf auf jeden Fall eine Database.xml Datei vorhanden ist
       public SQLCon()
@@ -25,9 +26,20 @@ namespace BiBo
 
       public bool createDatabase()
       {
-          string customerSQL = "CREATE TABLE IF NOT EXISTS Customer (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, firstName VARCHAR(100) NOT NULL, lastName VARCHAR(100) NOT NULL, birthDate DATETIME NOT NULL, street VARCHAR(100), streetNumber VARCHAR(10), additionalRoad VARCHAR(100), zipCode INTEGER(5), town VARCHAR(100), country VARCHAR(100), chargeAccount INTEGER(100));";
+          string customerSQL = "CREATE TABLE IF NOT EXISTS Customer (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, firstName VARCHAR(100) NOT NULL, lastName VARCHAR(100) NOT NULL, birthDate DATETIME, street VARCHAR(100), streetNumber VARCHAR(10), additionalRoad VARCHAR(100), zipCode INTEGER(5), town VARCHAR(100), country VARCHAR(100), chargeAccount INTEGER(100));";
           SQLiteCommand command = new SQLiteCommand(customerSQL, this.con);
           command.ExecuteNonQuery();
+          return true;
+      }
+
+      public bool addCustomer(Customer c)
+      {
+          if (c.FirstName != "")
+          {
+              SQLiteCommand command = new SQLiteCommand(this.con);
+              command.CommandText = "INSERT INTO Customer (id, firstName, lastName) VALUES (NULL,'" + c.FirstName + "','" + c.LastName + "');";
+              command.ExecuteNonQuery();
+          }
           return true;
       }
   }
