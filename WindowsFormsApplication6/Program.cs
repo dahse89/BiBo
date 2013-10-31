b46c50fed05c83d89371a0dc8d0025dc1aa1c054
marsteff <marsteff@users.noreply.github.com>
2013-10-31 12:02:48 +0100

    updated Program.cs to new usage
    think would be better to include the CreateTable method into constructor

--- WindowsFormsApplication6/Program.cs
+++ WindowsFormsApplication6/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using BiBo.Persons;
 using BiBo.Properties;
+using BiBo.DAO;
 
 namespace BiBo
 {
@@ -15,17 +16,35 @@ namespace BiBo
         [STAThread]
         static void Main()
         {
-          SQLCon con = new SQLCon(); 
+          //SQLCon con = new SQLCon(); 
           /*Application.EnableVisualStyles();
           Application.SetCompatibleTextRenderingDefault(false);
           Application.Run(new Form1());
 */
+          
+          SqlConnector<Book> conBook = SqlConnector<Book>.GetBookSqlInstance();
+          SqlConnector<Customer> conCustomer = SqlConnector<Customer>.GetCustomerSqlInstance();
+          
           Customer c = new Customer(123, "Marcus", "Münzberg", DateTime.Now);
-          con.createDatabase();
-          con.addCustomer(c);
+          Book b = new Book("Gronwoski", "SCJP6", "Programming");
+          
+          conCustomer.CreateTable();
+          conBook.CreateTable();
+    
+          conCustomer.AddEntry(c);
+          conBook.AddEntry(b);
 
-          List<Customer> customerList = con.getCustomer();
-          Console.WriteLine("Hallo");
+          List<Customer> customerList = conCustomer.GetAllEntrys();
+          List<Book> bookList = conBook.GetAllEntrys();
+          
+          foreach(Customer customer in customerList )
+          {
+	        	Console.WriteLine(customer);
+          }
+          foreach(Book book in bookList )
+          {
+	        	Console.WriteLine(book);
+	      }
           
         }
     }
