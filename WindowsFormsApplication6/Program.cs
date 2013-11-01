84fdbb6d0ab5a74e02eaa9b6f29e10913390230f
marsteff <marsteff@users.noreply.github.com>
2013-11-01 10:20:20 +0100

    clean Program.cs

--- WindowsFormsApplication6/Program.cs
+++ WindowsFormsApplication6/Program.cs
@@ -16,35 +16,15 @@ namespace BiBo
         [STAThread]
         static void Main()
         {
-          //SQLCon con = new SQLCon(); 
-          /*Application.EnableVisualStyles();
+        
+          SqlConnector<Customer> conCustomer = SqlConnector<Customer>.GetCustomerSqlInstance();
+          Customer c = new Customer(123, "Marcus", "Münzberg", DateTime(1989,07,16));
+          conCustomer.AddEntry(c);
+          
+          Application.EnableVisualStyles();
           Application.SetCompatibleTextRenderingDefault(false);
           Application.Run(new Form1());
-*/
-          
-          SqlConnector<Book> conBook = SqlConnector<Book>.GetBookSqlInstance();
-          SqlConnector<Customer> conCustomer = SqlConnector<Customer>.GetCustomerSqlInstance();
-          
-          Customer c = new Customer(123, "Marcus", "Münzberg", DateTime.Now);
-          Book b = new Book("Gronwoski", "SCJP6", "Programming");
-          
-          conCustomer.CreateTable();
-          conBook.CreateTable();
-    
-          conCustomer.AddEntry(c);
-          conBook.AddEntry(b);
 
-          List<Customer> customerList = conCustomer.GetAllEntrys();
-          List<Book> bookList = conBook.GetAllEntrys();
-          
-          foreach(Customer customer in customerList )
-          {
-	        	Console.WriteLine(customer);
-          }
-          foreach(Book book in bookList )
-          {
-	        	Console.WriteLine(book);
-	      }
           
         }
     }
