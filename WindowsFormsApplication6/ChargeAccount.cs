6f1e129204ad21733d4b291c745e21efb77c00a0
Marcus Münzberg <marsteff@gmail.com>
2013-11-08 20:13:20 +0100

    create ChargeAccount and ChargeAccountDAO classes

---
+++ WindowsFormsApplication6/ChargeAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiBo
+{
+    class ChargeAccount
+    {   
+        ulong customerID;
+        DateTime changedAt;
+        List<decimal>changeValues;
+        decimal currentValue;
+    }
+}
