8f451d9060d7ecb7f7bec1f44c909f6bf5d7213a
Vico Dambeck <vico.dambeck@gmail.com>
2013-11-12 12:37:20 +0100

    Fixed Obj

---
+++ WindowsFormsApplication6/CustomerSQL.cs
@@ -0,0 +1,202 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: m588
+ * Datum: 30.10.2013
+ * Zeit: 16:22
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Data.SQLite;
+using System.Data.SqlTypes;
+using BiBo.Persons;
+using System.Collections.Generic;
+
+
+namespace BiBo.SQL
+{
+	/// <summary>
+	/// Description of CustomerSql.
+	/// </summary>
+	public class CustomerSQL : SqlConnector<Customer>
+	{
+		public CustomerSQL()
+		{
+			string customerSQL = @"CREATE TABLE IF NOT EXISTS
+                                    Customer (
+                                        ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                        firstName VARCHAR(100) NOT NULL,
+                                        lastName VARCHAR(100) NOT NULL,
+                                        birthDate DATETIME,
+                                        street VARCHAR(100),
+                                        streetNumber VARCHAR(10),
+                                        additionalRoad VARCHAR(100), 
+                                        zipCode INTEGER(5), 
+                                        town VARCHAR(100), 
+                                        country VARCHAR(100), 
+                                        chargeAccount INTEGER(100)
+                                    );";
+
+          	SQLiteCommand command = new SQLiteCommand(customerSQL, con);
+          	command.ExecuteNonQuery();
+		}
+		
+		public override bool AddEntry(Customer customer)
+		{
+              SQLiteCommand command = new SQLiteCommand(con);
+              command.CommandText = @"INSERT INTO
+                                            Customer (
+                                                id,
+                                                firstName, 
+                                                lastName, 
+                                                birthDate,
+                                                street,
+                                                streetNumber,
+                                                additionalRoad,
+                                                zipCode,
+                                                town,
+                                                country
+                                            ) 
+                                            VALUES (
+                                                NULL,
+                                                '" + customer.FirstName + @"',
+                                                '" + customer.LastName  + @"',
+                                                '" + customer.BirthDate.ToShortDateString() + @"',
+                                                '" + customer.Street + @"',
+                                                '" + customer.StreetNumber + @"',
+                                                '" + customer.AdditionalRoad + @"',
+                                                '" + customer.ZipCode + @"',
+                                                '" + customer.Town + @"',
+                                                '" + customer.Country + @"'
+                                             );";
+              command.ExecuteNonQuery();
+
+            return true;
+		}
+
+        public override ulong AddEntryReturnId(Customer customer)
+        {
+            AddEntry(customer);//da hier ja eh ne unique id erzeugt wird kann ich die ja auch gleich nutzen <Philipp>
+
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "select last_insert_rowid()";
+            Int64 LastRowID64 = (Int64)command.ExecuteScalar();
+            return (ulong) LastRowID64;
+        }
+
+        public override bool DeleteEntryByIdList(List<ulong> l)
+        {
+            foreach(ulong x in l){
+                SQLiteCommand command = new SQLiteCommand(con);
+                command.CommandText = "DELETE FROM Customer WHERE id ='" + x + "';";
+                command.ExecuteNonQuery();
+              
+            }
+            return true;
+        }
+		
+		public override bool DeleteEntry(Customer customer)
+		{
+			if (customer.FirstName != "")
+            {
+              SQLiteCommand command = new SQLiteCommand(con);
+              command.CommandText = "DELETE FROM Customer WHERE id='" + customer.CustomerID + "';";
+              command.ExecuteNonQuery();
+            }
+            return true;
+		}
+
+        public override Customer GetEntryById(ulong cid){
+          SQLiteCommand command = new SQLiteCommand(con);
+          command.CommandText = "SELECT * FROM Customer WHERE id = '" + cid + "'";
+          SQLiteDataReader reader = command.ExecuteReader();
+          if (reader.HasRows)
+          {
+              reader.Read();
+              return InitEntryByReader(reader);
+          }
+
+          else
+              throw new Exception("Eintrag nicht vorhanden");//für mich sinnvoller als ein leeres Objekt zurück zugeben
+        }
+		
+		public override List<Customer> GetAllEntrys()
+		{
+		List<Customer> customerList = new List<Customer>();
+
+          SQLiteCommand command = new SQLiteCommand(con);
+          command.CommandText = "SELECT * FROM Customer";
+          SQLiteDataReader reader = command.ExecuteReader();
+
+          if (reader.HasRows)
+          {
+              while (reader.Read())
+              {
+                  customerList.Add(InitEntryByReader(reader));
+              }
+          }
+
+          return customerList;
+		}
+
+        protected override Customer InitEntryByReader(SQLiteDataReader reader){
+            Customer tmp;
+
+            int intId = reader.GetInt32(reader.GetOrdinal("id"));
+            ulong id = System.Convert.ToUInt64(intId);
+            string firstName = reader.GetString(reader.GetOrdinal("firstName"));
+            string lastName = reader.GetString(reader.GetOrdinal("lastName"));
+            DateTime birthDate = DateTime.Parse(reader.GetString(reader.GetOrdinal("birthDate")));
+
+            tmp = new Customer(id, firstName, lastName, birthDate);
+                  
+            string street = reader.GetString(reader.GetOrdinal("street"));
+            tmp.Street = street;
+            tmp.StreetNumber = Convert.ToInt32(reader.GetString(reader.GetOrdinal("streetNumber")));
+            tmp.AdditionalRoad = reader.GetString(reader.GetOrdinal("additionalRoad"));
+            tmp.ZipCode = reader.GetInt32(reader.GetOrdinal("zipCode"));
+            tmp.Town = reader.GetString(reader.GetOrdinal("town"));
+            tmp.Country = reader.GetString(reader.GetOrdinal("country"));
+
+            return tmp;
+        }
+		
+		bool BorrowBook(ulong bookId)
+		{
+			return true;
+		}
+		
+		bool NoBooksBorrowed()
+		{
+			return true;
+		}
+		
+		bool HasOwe()
+		{
+			return true;
+		}
+		
+		void MergeBlanaceWith(int value)
+		{
+			
+		}
+		
+		void CreateIDCard()
+		{
+			
+		}
+		
+		void ExtendIDCard()
+		{
+			
+		}
+		
+		void OrderBookInAdvance(ulong bookId)
+		{
+			
+		}
+		
+	
+		
+	}
+}
---
+++ WindowsFormsApplication6/ExemplarSQL.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using BiBo.Persons;
+
+using BiBo.SQL;
+
+namespace BiBo.SQL
+{
+    public class ExemplarSQL : SqlConnector<Exemplar>
+    {
+        //Member-Variablen Deklaration
+        private DateTime loanPeriod;     //Ausleifrist 
+        private BookStates state;		    //Status des Buches (only_visible, damaged, missing)
+        private string signatur; 	    //signatur des buches
+        private Access accesser; 	    //Zugang zum Exemplar (magazin, freihandausleihe)
+        private Customer borrower;	    //Ausleiher
+        private ulong exemplarId;     //Exemplar-Nummer
+        private ulong bookId;         //dazugehörige Buch-ID
+    
+        public ExemplarSQL()
+        {
+            string exemplarSQL = @"CREATE TABLE IF NOT EXISTS Exemplar (
+                                       ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  
+                                       loanPeriod DateTime, 
+                                       state VARCHAR(100) NOT NULL,
+                                       signatur VARCHAR(100) NOT NULL, 
+                                       access VARCHAR(100) NOT NULL,
+                                       customerID INTEGER,
+                                       bookID INTEGER
+                                    );";
+
+            SQLiteCommand command = new SQLiteCommand(exemplarSQL, con);
+            command.ExecuteNonQuery();
+        }
+
+        public override bool AddEntry(Exemplar exemplar)
+        {
+            SQLiteCommand command = new SQLiteCommand(con);
+            if(command != null){
+            command.CommandText = @"INSERT INTO Exemplar (
+                                      id, 
+                                      loanPeriod,
+                                      state, 
+                                      signatur, 
+                                      access,
+                                      customerID,
+                                      bookID
+                                  )   
+                                  VALUES (
+                                      NULL,  
+                                      '" + exemplar.LoanPeriod.ToShortDateString() 	+ @"',
+                                      '" + exemplar.State.ToString()				+ @"',
+                                      '" + exemplar.Signatur 						+ @"',
+                                      '" + exemplar.Accesser.ToString() 			+ @"',
+                                      '" + "5" 										+ @"', 
+                                      '" + exemplar.BookId.ToString() 				+ @"'
+                                  );";
+
+            command.ExecuteNonQuery();
+            }
+            return true;
+        }
+
+        public override ulong AddEntryReturnId(Exemplar exemplar)
+        {
+            AddEntry(exemplar);
+
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "select last_insert_rowid()";
+            UInt64 lastRowID64 = (UInt64)command.ExecuteScalar();
+            return (ulong)lastRowID64;
+        }
+
+        public override bool DeleteEntry(Exemplar exemplar)
+        {
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "DELETE FROM Exemplar WHERE id = '" + exemplar.ExemplarId + "';";
+            command.ExecuteNonQuery();
+            return true;
+        }
+
+        public override bool DeleteEntryByIdList(List<ulong> l)
+        {
+            foreach (ulong x in l)
+            {
+                SQLiteCommand command = new SQLiteCommand(con);
+                command.CommandText = "DELETE FROM Customer WHERE id = '" + x + "';";
+                command.ExecuteNonQuery();
+
+            }
+            return true;
+        }
+
+        public override Exemplar GetEntryById(ulong id)
+        {
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "SELECT * FROM Exemplar WHERE id = '" + id + "'";
+            SQLiteDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                return InitEntryByReader(reader);
+            }
+
+            else
+                throw new Exception("Eintrag nicht vorhanden");
+        }
+
+        public override List<Exemplar> GetAllEntrys()
+        {
+            List<Exemplar> exemplarList = new List<Exemplar>();
+
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "SELECT * FROM Exemplar";
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    exemplarList.Add(InitEntryByReader(reader));
+                }
+            }
+
+            return exemplarList;
+        }
+
+        protected override Exemplar InitEntryByReader(System.Data.SQLite.SQLiteDataReader reader)
+        {
+            Exemplar exemplar = new Exemplar();
+            
+            ulong id = System.Convert.ToUInt64(reader.GetInt32(reader.GetOrdinal("id")));
+
+            string loanPeriodAsString = reader.GetString(reader.GetOrdinal("loanPeriod"));
+            DateTime loanPeriod = new DateTime();
+            if(loanPeriodAsString != null || loanPeriodAsString != "")
+                loanPeriod = DateTime.Parse(loanPeriodAsString);
+
+            string stateString = reader.GetString(reader.GetOrdinal("state"));
+            BookStates state = (BookStates) Enum.Parse(typeof(BookStates), stateString, true);
+
+            string signatur = reader.GetString(reader.GetOrdinal("signatur"));
+            ulong customerId = System.Convert.ToUInt64(reader.GetInt32(reader.GetOrdinal("customerID")));
+            ulong bookId = System.Convert.ToUInt64(reader.GetInt32(reader.GetOrdinal("bookID")));
+            
+            exemplar.ExemplarId = id;
+            exemplar.LoanPeriod = loanPeriod;
+            exemplar.State = state;
+            exemplar.Signatur = signatur;
+            exemplar.BookId = bookId;
+
+            return exemplar;
+        }
+
+        //to make it possile to call the method outside
+        public Exemplar GetInitEntryByReader(SQLiteDataReader reader)
+        {
+            return InitEntryByReader(reader);
+        }
+
+        //new methods TODO : IMPLEMENT THIS SHIT
+
+        
+        public void ExtendLoanPeriodTo(DateTime dateBookWillBeBack) //<--- object von exemplar muss mit rein
+        {
+        }
+
+        public void ReduceLoanPeriodTo(DateTime dateBookWillBeBack) //<--- object von exemplar muss mit rein
+        {
+        }
+
+        public bool Borrow(DateTime dateBookWillBeBack, String cardId) //<--- object von exemplar muss mit rein
+        {
+            return true;
+        }
+    }   
+}
