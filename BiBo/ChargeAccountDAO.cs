9e0a8fc40f55cc09f38918ec64893096c658b30f
dahse89 <ph.dahse@gmail.com>
2013-11-25 12:05:38 +0100

    start with WPF, User add GUI done

---
+++ BiBo/ChargeAccountDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BiBo.SQL;
+
+namespace BiBo.DAO
+{
+  public class ChargeAccountDAO
+  {
+    MainWindow GUI;
+    //public ChargeAccountSQL chargeAccountSql = SqlConnector<ChargeAccount>.GetChargeAccountSqlInstance();
+
+    public ChargeAccountDAO(MainWindow GUI)
+    {
+      this.GUI = GUI;
+    }
+  }
+}
---
+++ BiBo/TestDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiBo.DAO
+{
+  public class TestDAO
+  {
+    private MainWindow GUI;
+
+    public TestDAO(MainWindow gui)
+    {
+      this.GUI = gui;
+    }
+   
+    public void changeLoggedUserName(String name)
+    {
+      //db
+      //obj
+      //this.GUI.SetLoggedUserName(name);
+    }
+
+  }
+}
