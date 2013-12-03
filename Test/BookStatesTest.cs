80354e237d588b5e2dc1df2bc296867971d187aa
Vico Dambeck <vico.dambeck@gmail.com>
2013-12-03 13:08:44 +0100

    Merge

---
+++ Test/BookStatesTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiBo;
+using System;
+
+namespace Test
+{
+    [TestClass()]
+    public class BookStatesTest
+    {
+        [TestMethod()]
+        public void BookStateTest()
+        {
+            ulong bookId = 1;                           // ID
+            string author = "Mueller";       			//Autor des Buch
+            string titel = "Das Buch";                  //Titel
+            string subjectArea = "Drama";  			    //Fachrichtung
+
+            Book buch = new Book(bookId, author, titel, subjectArea);
+            Exemplar ex = new Exemplar(buch);
+
+            ex.State = Converter.ToBookState("only_visible");
+            Assert.IsTrue(ex.State == BookStates.ONLY_VISIBLE);
+
+            ex.State = Converter.ToBookState("damaged");
+            Assert.IsTrue(ex.State == BookStates.DAMAGED);
+
+            ex.State = Converter.ToBookState("missing");
+            Assert.IsTrue(ex.State == BookStates.MISSING);
+
+            ex.State = Converter.ToBookState("ok");
+            Assert.IsTrue(ex.State == BookStates.OK);
+
+         
+        }
+    }
+}
---
+++ Test/BookTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiBo;
+using System;
+
+namespace Test
+{
+    [TestClass()]
+    public class BookTest
+    {
+        [TestMethod()]
+        public void CreateSignaturTest()
+        {
+            ulong bookId = 1;                           // ID
+            string author = "Mueller";       			//Autor des Buch
+            string titel = "Das Buch";                  //Titel
+            string subjectArea = "Drama";  			    //Fachrichtung
+
+            Book boo = new Book(bookId, author, titel, subjectArea);
+
+            Assert.IsTrue(boo.BookId == bookId);
+            Assert.IsTrue(boo.Author == author);
+            Assert.IsTrue(boo.Titel == titel);
+            Assert.IsTrue(boo.SubjectArea == subjectArea);
+
+            string authorPrefix = author[0].ToString();
+            string subjectAreaPrefix = subjectArea.Substring(0, 3);
+            string signatur = "[" + authorPrefix + subjectAreaPrefix + "]";
+
+            Assert.AreEqual(boo.CreateSignatur(), signatur);
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            ulong bookId = 1;                           // ID
+            string author = "Mueller";       			//Autor des Buch
+            string titel = "Das Buch";                  //Titel
+            string subjectArea = "Drama";  			    //Fachrichtung
+
+            Book boo = new Book(bookId, author, titel, subjectArea);
+
+            Assert.IsTrue(boo.BookId == bookId);
+            Assert.IsTrue(boo.Author == author);
+            Assert.IsTrue(boo.Titel == titel);
+            Assert.IsTrue(boo.SubjectArea == subjectArea);
+
+            string sString = author + " " + titel + " " + subjectArea;
+
+            Assert.AreEqual(boo.ToString(), sString);
+        }
+
+
+    }
+}
