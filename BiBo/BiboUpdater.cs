4aa4722c70c735d0e2dbcd584439cce6f3575806
Vico Dambeck <vico.dambeck@gmail.com>
2013-12-05 23:03:13 +0100

    Added AutoUpdater

---
+++ BiBo/BiboUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Deployment.Application;
+
+namespace BiBo.Updater
+{
+    class BiboUpdater
+    {
+        public Version AssemblyVersion
+        {
+            get
+            {
+                try
+                {
+                    return ApplicationDeployment.CurrentDeployment.CurrentVersion;
+                }
+                catch
+                {
+                    return new Version("1.3.3.7");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return ""+this.AssemblyVersion;
+        }
+    }
+}
